794b2ea2a0982a3b8eaba8cc9cdcca37e2ad4a17
Pavel <pavel366@yandex.ru>
2018-04-28 13:34:17 +0300

    initial

---
+++ app/src/main/java/com/example/ios_razrab/firstguide2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ios_razrab.firstguide2;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.wooplr.spotlight.SpotlightView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView =(TextView)findViewById(R.id.tv);
+
+
+        new SpotlightView.Builder(this)
+                .introAnimationDuration(400)
+                .performClick(true)
+                .fadeinTextDuration(400)
+                .headingTvColor(Color.parseColor("#eb273f"))
+                .headingTvSize(32)
+                .headingTvText("Love")
+                .subHeadingTvColor(Color.parseColor("#ffffff"))
+                .subHeadingTvSize(16)
+                .subHeadingTvText("Like the picture?\nLet others know.")
+                .maskColor(Color.parseColor("#dc000000"))
+                .target(textView)
+                .lineAnimDuration(400)
+                .lineAndArcColor(Color.parseColor("#eb273f"))
+                .dismissOnTouch(true)
+                .dismissOnBackPress(true)
+                .enableDismissAfterShown(true)
+                .usageId("usageId") //UNIQUE ID
+                .show();
+
+    }
+}
